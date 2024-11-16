c2ddc95b33c337d41b2f64af293d89d992ddc1f2
𝄟✮⃝❤𝗛𝗜×͜×𝗠𝗨✮⃝❤𝄟 <166631945+HIMU999FF@users.noreply.github.com>
2024-11-16 16:58:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ module.exports.alldown = (url) =>
 
       resolve({
         status: true,
-        dev: "Your Name",
-        devfb: "Your fb account",
-        devwp: "wp number", 
+        dev: "404 (HIMU)",
+        devfb: "https://www.facebook.com/99.6T9.HIMU.6T9.1?mibextid=ZbWKwL",
+        devwp: "w.me/+31613501164", 
         data: data || msg,
       });
     } catch (error) {
